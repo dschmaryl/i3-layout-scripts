70e9ceab2dffee9bf51979ea810228291d4e09a6
dschmaryl <dschmaryl@gmail.com>
2017-08-08 00:50:43 -0400

    add workspace number check

--- open.py
+++ open.py
@@ -1,43 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import subprocess
-import sys
+from os.path import expanduser, isfile
+from subprocess import check_output
+from sys import argv
 
 
-LAYOUTS_DIR = os.path.expanduser('~') + '/.config/i3/layouts/'
+LAYOUTS_DIR = expanduser('~') + '/.config/i3/layouts/'
 
 
-def open_layout(workspace_name, workspace_number=None):
-    executables_file = LAYOUTS_DIR + workspace_name
+def open_layout(layout_name, workspace_number=None):
+    executables_file = LAYOUTS_DIR + layout_name
     layout_file = executables_file + '.json'
 
-    if not os.path.isfile(layout_file):
-        print('layout not found: ' + workspace_name)
+    if not isfile(layout_file):
+        print('layout not found: ' + layout_name)
         return
-    if not os.path.isfile(executables_file):
-        print('executable not found: ' + workspace_name)
+    if not isfile(executables_file):
+        print('executable not found: ' + layout_name)
         return
 
     if workspace_number:
-        subprocess.check_output(['i3-msg', 'workspace', workspace_number])
+        check_output(['i3-msg', 'workspace', str(workspace_number)])
 
-    subprocess.check_output(['i3-msg', 'append_layout', layout_file])
-    subprocess.check_output([executables_file])
+    check_output(['i3-msg', 'append_layout', layout_file])
+    check_output([executables_file])
+
+
+def invalid_workspace(workspace_number):
+    try:
+        if int(workspace_number) not in range(1, 11):
+            raise ValueError
+    except ValueError:
+        return True
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        workspace_name = sys.argv[-2]
-        workspace_number = sys.argv[-1]
-        if int(workspace_number) not in range(1, 11):
+    if len(argv) == 3:
+        workspace_number = argv[-1]
+        if invalid_workspace(workspace_number):
             exit(workspace_number + ' is not a valid workspace')
-        open_layout(workspace_name, workspace_number)
 
-    elif len(sys.argv) == 2:
-        workspace_name = sys.argv[-1]
-        open_layout(workspace_name)
+        open_layout(argv[-2], workspace_number)
+
+    elif len(argv) == 2:
+        open_layout(argv[-1])
 
     else:
-        exit("usage: open LAYOUT_NAME WORKSPACE_NUMBER")
+        exit("usage: open LAYOUT_NAME [WORKSPACE_NUMBER]")
