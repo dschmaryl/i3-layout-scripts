0babfc8ade28abdcb5de10aba29da1920244c3c4
dschmaryl <dschmaryl@gmail.com>
2018-02-12 13:44:45 -0500

    dont list bin dir as layout, fix config

--- config.py
+++ config.py
@@ -6,9 +6,8 @@ from os.path import expanduser
 
 # Default location is '~/.i3-layouts'. Change according to preference.
 # LAYOUTS_DIR must end with '/'
-# LAYOUTS_DIR = expanduser('~') + '/.i3-layouts/'
-LAYOUTS_DIR = expanduser('~') + '/.i3-layouts-test/'
+LAYOUTS_DIR = expanduser('~') + '/.i3-layouts/'
 
 # BIN_DIR is the folder in which shortcuts to open layouts are saved. Make
 # sure this directory is in your PATH. Must end with '/'
-BIN_DIR = expanduser('~') + '/.i3-layouts-test/bin/'
+BIN_DIR = expanduser('~') + '/.i3-layouts/bin/'
--- list.py
+++ list.py
@@ -10,5 +10,9 @@ layouts = sorted([f for f in listdir(LAYOUTS_DIR) if '.json' not in f])
 
 print('')
 for layout in layouts:
-    print(layout)
+    if layout == 'bin':
+        # don't list bin dir as layout
+        continue
+    else:
+        print(layout)
 print('')
