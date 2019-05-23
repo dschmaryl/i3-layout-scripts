a5560a47a1d7ec895efb5241a9ccb802cc62b65d
dschmaryl <dschmaryl@gmail.com>
2019-05-23 12:37:03 -0400

    fix clean up

--- list.py
+++ list.py
@@ -4,14 +4,8 @@ from os import listdir
 
 from config import LAYOUTS_DIR
 
-
-def get_layouts():
-    return sorted([f.split('.json')[0]
-                   for f in listdir(LAYOUTS_DIR) if '.json' in f])
-
-
-if __name__ == '__main__':
-    print('')
-    for layout in get_layouts():
-        print(layout)
-    print('')
+print('')
+for file_name in sorted(listdir(LAYOUTS_DIR)):
+    if '.json' in file_name:
+        print(file_name.split('.json')[0])
+print('')
--- remove.py
+++ remove.py
@@ -3,25 +3,25 @@
 from os import listdir, remove
 from sys import argv
 
-from list import get_layouts
 from config import CREATE_BIN_SHORTCUTS, BIN_DIR, LAYOUTS_DIR
 
 
 def remove_layout(layout_name):
-    remove(LAYOUTS_DIR + layout_name)
-    remove(LAYOUTS_DIR + layout_name + '.json')
-    if CREATE_BIN_SHORTCUTS:
-        remove(BIN_DIR + layout_name)
+    layouts = [f.split('.json')[0]
+               for f in listdir(LAYOUTS_DIR) if '.json' in f]
+
+    if layout_name in layouts:
+        remove(LAYOUTS_DIR + layout_name)
+        remove(LAYOUTS_DIR + layout_name + '.json')
+        if CREATE_BIN_SHORTCUTS:
+            remove(BIN_DIR + layout_name)
+            print("layout '" + layout_name + "' removed")
+    else:
+        print("no layout named '" + layout_name + "'")
 
 
 if __name__ == '__main__':
     if len(argv) != 2:
         print("usage: remove LAYOUT_NAME")
     else:
-        layout_name = argv[-1]
-        layouts = get_layouts()
-        if layout_name in layouts:
-            remove_layout(layout_name)
-            print("layout '" + layout_name + "' removed")
-        else:
-            print("no layout named '" + layout_name + "'")
+        remove_layout(layout_name)
